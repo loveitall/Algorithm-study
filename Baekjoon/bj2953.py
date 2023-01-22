41e003a32719fe87d36da49b94f121a32f68dc85
CHO_noori <93557885+loveitall@users.noreply.github.com>
2023-01-22 20:14:16 +0900

    [bj 2935] 나는 요리사다2
    다른 풀이도 생각해보았다

--- Baekjoon/bj2953.py
+++ Baekjoon/bj2953.py
@@ -1,10 +1,27 @@
 ## 나는 요리사다 문제
-score = []
+
+#  첫번째 풀이 방법
+# score = []
+
+# for i in range(5):
+#     score.append(sum(map(int, input().split())))
+
+# print(score.index(max(score))+1, max(score))
+
+
+# 2번째 풀이 방법
+# 이건 좀 더 생각 흐름대로 짠 부분
+maxScore= 0
+maxIndex = 0
 
 for i in range(5):
-    score.append(sum(map(int, input().split())))
+    s1, s2, s3, s4 = map(int, input().split())
+    score = s1 + s2 + s3 + s4
+    if maxScore < score :
+        maxScore = score
+        maxIndex = i + 1
 
-print(score.index(max(score))+1, max(score))
+print(maxIndex, maxScore)
 
 # 결과 실행창
 # 5 4 4 5
@@ -13,3 +30,5 @@ print(score.index(max(score))+1, max(score))
 # 5 5 5 4
 # 4 4 4 5
 # 4 19
+
+
